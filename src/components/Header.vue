7b592562e973d794c6f5b725df27f5054ccd472c
YinH <1691116129@qq.com>
2025-02-18 01:06:18 +0800

    chore:finsh

---
+++ src/components/Header.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="header">
+    <div class="left">
+      <!-- 是否进入多级table显示返回按钮 -->
+      <el-icon class="back" v-if="state.hasBack" @click="back"
+        ><Back
+      /></el-icon>
+      <span style="font-size: 20px">{{ state.name }}</span>
+    </div>
+    <div class="right">
+      <el-popover
+        placement="bottom"
+        :width="320"
+        trigger="click"
+        popper-class="popper-user-box"
+      >
+        <!-- el-popover提供了插槽 -->
+        <template #reference>
+          <div class="author">
+            <el-icon><Avatar /></el-icon>
+            {{ (state.userInfo && state.userInfo.nickName) || "" }}
+            <el-icon><CaretBottom /></el-icon>
+          </div>
+        </template>
+        <div class="nickname">
+          <p>
+            登录名：{{ (state.userInfo && state.userInfo.loginUserName) || "" }}
+          </p>
+          <p>昵称：{{ (state.userInfo && state.userInfo.nickName) || "" }}</p>
+          <el-tag size="small" effect="dark" class="logout" @click="logout"
+            >退出</el-tag
+          >
+        </div>
+      </el-popover>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<script setup>
+import { onMounted, reactive, toRefs } from "vue";
+import { useRouter } from "vue-router";
+import axios from "@/utils/axios";
+import { localRemove, pathMap } from "@/utils";
+const router = useRouter();
+const state = reactive({
+  name: "dashboard",
+  userInfo: null, // 用户信息变量
+  hasBack: false, // 是否展示返回icon
+});
+// 初始化执行方法
+onMounted(() => {
+  const pathname = window.location.hash.split("/")[1] || "";
+  if (!["login"].includes(pathname)) {
+    getUserInfo();
+  }
+});
+// 获取用户信息
+const getUserInfo = async () => {
+  const userInfo = await axios.get("/adminUser/profile");
+  state.userInfo = userInfo;
+};
+// 退出登录
+const logout = () => {
+  axios.delete("/logout").then(() => {
+    // 退出之后，将本地保存的 token  清理掉
+    localRemove("token");
+    // 回到登录页
+    router.push({ path: "/login" });
+  });
+}
+
+router.afterEach((to) => {
+  console.log("to", to);
+  const { id } = to.query;
+  state.name = pathMap[to.name];
+  // level2 和 level3 需要展示返回icon
+  state.hasBack = ["level2", "level3"].includes(to.name);
+})
+
+const back = () => {
+  router.back();
+}
+</script>
+
+<style scoped>
+.header {
+  height: 50px;
+  border-bottom: 1px solid #e9e9e9;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+}
+.right > div > .icon {
+  font-size: 18px;
+  margin-right: 6px;
+}
+.author {
+  margin-left: 10px;
+  cursor: pointer;
+}
+
+.header .left .back {
+  border: 1px solid #e9e9e9;
+  padding: 5px;
+  border-radius: 50%;
+  margin-right: 5px;
+  cursor: pointer;
+}
+
+</style>
+
+<style>
+.popper-user-box {
+  background: url("https://s.yezgea02.com/lingling-h5/static/account-banner-bg.png")
+    50% 50% no-repeat !important;
+  background-size: cover !important;
+  border-radius: 0 !important;
+}
+.popper-user-box .nickname {
+  position: relative;
+  color: #ffffff;
+}
+.popper-user-box .nickname .logout {
+  position: absolute;
+  right: 0;
+  top: 0;
+  cursor: pointer;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,52 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "@/router";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
+const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+// 全局方法
+app.config.globalProperties.$filters = {
+  prefix(url) {
+    if (url && url.startsWith("http")) {
+      // 当 url 以 http 开头时候，我们返回原路径
+      return url;
+    } else {
+      // 否则，我们给路径添加 host，如下
+      url = `http://backend-api-02.newbee.ltd${url}`;
+      return url;
+    }
+  },
+};
+
+const orderStatus = {
+  0: '待支付',
+  1: '已支付',
+  2: '配货完成',
+  3: '出库成功',
+  4: '交易成功',
+  '-1': '手动关闭',
+  '-2': '超时关闭',
+  '-3': '商家关闭'
+}
+
+// 全局方法
+app.config.globalProperties.$filters = {
+  orderMap(status) {
+    return orderStatus[status] || '未知状态'
+  },
+  prefix(url) {
+    if (url && url.startsWith('http')) {
+      return url
+    } else {
+      url = `http://backend-api-02.newbee.ltd${url}`
+      return url
+    }
+  }
+}
+
+app.use(router);
+
+app.mount("#app");
